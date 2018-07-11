ec0326085581684ca14cfee1746ae2cee268a4dd
DESKTOP-A3R12Q7\admin <toptal.alltime@gmail.com>
2018-07-11 11:33:16 -0400

    Send first order to MA portal.

--- tests/integration/LoginTest.php
+++ tests/integration/LoginTest.php
@@ -44,11 +44,56 @@ class LoginTest extends PHPUnit_Extensions_Selenium2TestCase
     public function testLoginSuccessful()  {
         $this->timeouts()->implicitWait(2000);
 
-            $path = 'results.json';
-            $fp = fopen($path, 'r');
-            $fileData = fread($fp, filesize($path));
-            $json = json_decode($fileData, true);
-            fclose($fp);  
+        $data_string = "username=smartdev2017&password=smartdev2017";
+        $old_url = 'https://mayflowersandbox.navigatorpos.com/api/v3';
+        $new_url = 'https://nbs-dev.azurewebsites.net/api/v3';
+        
+        $api_url =  $old_url;
+        
+        $ch  = curl_init();
+    
+        function curl_request($ch, $url, $post_data, $token) {
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);        
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_HEADER, 1);
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+            if ($post_data != null) {
+                curl_setopt($ch, CURLOPT_POST, 1);
+                curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
+            } else {
+                curl_setopt($ch, CURLOPT_POST, 0);
+            }
+            if ($token != null) {
+                curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+                    "Authorization: Bearer ".$token,
+                    'Content-Type: application/json'
+                ));
+            }
+            // Get content
+            $page = curl_exec($ch);
+            $header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+            $header = substr($page, 0, $header_size);
+            $body = substr($page, $header_size);
+    
+            return $body;
+        }
+    
+        $auth = curl_request($ch, $api_url.'/token', $data_string, null);
+        $auth_obj = json_decode($auth);
+        $token = $auth_obj->token;
+        $customers = curl_request($ch, $api_url.'/customers', null, $token);
+    
+        $customersInfo =json_decode($customers, true);
+        $totalOrders = [];
+        foreach ($customersInfo["data"] as $eachCustomer) {
+            if ($eachCustomer["ispatient"]) {
+                $orders = curl_request($ch, $api_url.'/order'.'?customerID='.$eachCustomer["id"], null, $token);
+                $ordersInfo =json_decode($orders, true);
+                $totalOrders = $ordersInfo["data"];
+            }
+        } 
+        $json = $totalOrders[0];
   
     try {
 
